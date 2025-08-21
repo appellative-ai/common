ecd6617c77dc98d0aa274ce17b0647869092563f
appellative-ai <mark.wilson.ai@outlook.com>
2025-08-21 12:37:16 -0500

    Initial commit

---
+++ core/map.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"sync"
+)
+
+// MapT - map type
+type MapT[T, U any] struct {
+	m *sync.Map
+}
+
+// NewSyncMap - create a new map
+func NewSyncMap[T, U any]() *MapT[T, U] {
+	c := new(MapT[T, U])
+	c.m = new(sync.Map)
+	return c
+}
+
+func (m *MapT[T, U]) Load(t T) (u U, ok bool) {
+	v, ok1 := m.m.Load(t)
+	if !ok1 {
+		return u, ok1
+	}
+	if v1, ok2 := v.(U); ok2 {
+		return v1, ok2
+	}
+	return u, false
+}
+
+func (m *MapT[T, U]) Store(t T, u U) {
+	m.m.Store(t, u)
+}
---
+++ core/map_test.go
@@ -0,0 +1,66 @@
+package core
+
+import (
+	"fmt"
+)
+
+type agent interface {
+	Name() string
+}
+
+type newAgentFunc func() agent
+
+type address struct {
+	Address string
+	City    string
+	State   string
+}
+
+func ExampleNewMap() {
+	m := NewSyncMap[string, newAgentFunc]()
+	name1 := ""
+	t, ok := m.Load("")
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t, ok)
+
+	name1 = "common:core:ctor/test"
+	m.Store(name1, nil)
+	t, ok = m.Load(name1)
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t, ok)
+
+	m.Store(name1, func() agent { return nil })
+	t, ok = m.Load(name1)
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t != nil, ok)
+
+	name1 = "common:core:ctor/invalid"
+	t, ok = m.Load(name1)
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t != nil, ok)
+
+	//Output:
+	//test:  Load("") -> <nil> [ok:false]
+	//test:  Load("common:core:ctor/test") -> <nil> [ok:true]
+	//test:  Load("common:core:ctor/test") -> true [ok:true]
+	//test:  Load("common:core:ctor/invalid") -> false [ok:false]
+
+}
+
+func ExampleNewMap_Address() {
+	m := NewSyncMap[string, address]()
+	name1 := ""
+	t, ok := m.Load(name1)
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t, ok)
+
+	name1 = "common:core:ctor/test"
+	m.Store(name1, address{Address: "123 Main", City: "Anytown", State: "Ohio"})
+	t, ok = m.Load(name1)
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t, ok)
+
+	name1 = "common:core:ctor/invalid"
+	t, ok = m.Load(name1)
+	fmt.Printf("test:  Load(\"%v\") -> %v [ok:%v]\n", name1, t, ok)
+
+	//Output:
+	//test:  Load("") -> {  } [ok:false]
+	//test:  Load("common:core:ctor/test") -> {123 Main Anytown Ohio} [ok:true]
+	//test:  Load("common:core:ctor/invalid") -> {  } [ok:false]
+
+}
