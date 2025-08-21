a94613c4b5a20dc8674fb0682234bdc941687566
appellative-ai <mark.wilson.ai@outlook.com>
2025-08-21 15:27:46 -0500

    Updating

---
+++ core/status_test.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func ExampleStatus() {
+	s := NewStatus(http.StatusTeapot, errors.New("this is an error")).SetLocation("agent\test")
+
+	fmt.Printf("test: NewStatus() -> %v\n", s)
+
+	fmt.Printf("test: NewStatus() -> %v\n", StatusOK)
+
+	fmt.Printf("test: NewStatus() -> %v\n", StatusNotFound)
+
+	//Output:
+	//test: NewStatus() -> I'm A Teapot - this is an error
+	//test: NewStatus() -> OK
+	//test: NewStatus() -> Not Found
+
+}
---
+++ httpx/concurrent.go
@@ -0,0 +1,72 @@
+package httpx
+
+import (
+	"github.com/appellative-ai/common/core"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type LogFunc func(start time.Time, duration time.Duration, req *http.Request, resp *http.Response, timeout time.Duration)
+
+type ConcurrentResult interface {
+	Get(name string) *ExchangeResponse
+}
+
+type ExchangeInvoke struct {
+	Name    string
+	Timeout time.Duration
+	Do      core.Exchange
+	Req     *http.Request
+}
+
+type ExchangeResponse struct {
+	Resp *http.Response
+	Err  error
+}
+
+func DoConcurrent(invokes []ExchangeInvoke, logger LogFunc) ConcurrentResult {
+	var wg sync.WaitGroup
+
+	cnt := len(invokes)
+	m := newResults()
+	for i := 0; i < cnt; i++ {
+		wg.Add(1)
+		go func(i *ExchangeInvoke) {
+			defer wg.Done()
+			start := time.Now().UTC()
+			resp, err := Do(i.Req)
+			if logger != nil {
+				logger(start, time.Since(start), i.Req, resp, i.Timeout)
+			}
+			m.put(i.Name, &ExchangeResponse{Resp: resp, Err: err})
+		}(&invokes[i])
+	}
+	wg.Wait()
+	return m
+}
+
+type results struct {
+	m *sync.Map
+}
+
+func newResults() *results {
+	c := new(results)
+	c.m = new(sync.Map)
+	return c
+}
+
+func (e *results) Get(name string) *ExchangeResponse {
+	value, ok := e.m.Load(name)
+	if !ok {
+		return nil
+	}
+	if value1, ok1 := value.(*ExchangeResponse); ok1 {
+		return value1
+	}
+	return nil
+}
+
+func (e *results) put(name string, r *ExchangeResponse) {
+	e.m.Store(name, r)
+}
