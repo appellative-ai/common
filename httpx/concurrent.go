682ac0b624648d80ea2dba16b694cfe0fc1d8727
appellative-ai <mark.wilson.ai@outlook.com>
2025-08-22 09:25:19 -0500

    Update concurrent.go

--- httpx/concurrent.go
+++ httpx/concurrent.go
@@ -31,18 +31,15 @@ func newResult(p Params) *Result {
 	return r
 }
 
-func DoConcurrent[T Logger](do func(req *http.Request) (*http.Response, error), params ...Params) *core.MapT[string, *Result] {
+func DoConcurrent(do func(req *http.Request) (*http.Response, error), params ...Params) *core.MapT[string, *Result] {
 	var wg sync.WaitGroup
-	var t T
 
 	m := core.NewSyncMap[string, *Result]()
 	for _, p := range params {
 		wg.Add(1)
 		go func(r *Result) {
 			defer wg.Done()
-			start := time.Now().UTC()
 			r.Resp, r.Err = do(r.Req)
-			t.Log(start, time.Since(start), r.Name, r.Req, r.Resp, timeout(r.Req.Context()))
 			m.Store(r.Name, r)
 		}(newResult(p))
 	}
